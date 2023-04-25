84151d8151ab57816ea914a2c3618ffd446d6fe4
Marty Kinter <martykinter@Martys-Air.attlocal.net>
2023-04-24 20:25:43 -0400

    made corrections to broken-app and moved package.json to broken app folder

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const ExpressError = require('./expressError');
+
+app.use(express.json());
 
 
 async function getGithubData(req, res, next) {
   try {
-    const results = await Promise.all(req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    }));
+    const requests = req.body.developers.map(d => axios.get(`https://api.github.com/users/${d}`));
+    const results = await Promise.all(requests);
     const out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
     req.githubData = out;
     next();
@@ -17,8 +19,26 @@ async function getGithubData(req, res, next) {
 };
 
 app.post('/', getGithubData, function(req, res) {
-  res.send(JSON.stringify(req.githubData));
+  try{
+    res.send(JSON.stringify(req.githubData));
+  }catch(err){
+    next(err);
+  }
 });
 
+//invalid route
+app.use(function(req, res, next){
+  const err = new ExpressError("Not found", 404);
+  return next(err);
+});
+
+//error handler
+app.use(function(err, req, res, next){
+  res.status(err.status || 500);
+
+  return res.json({
+      error: err.message
+  });
+});
 
 app.listen(3000);
---
+++ broken-app/expressError.js
@@ -0,0 +1,11 @@
+//custom error handler with extended information
+class ExpressError extends Error{
+    constructor(message, status){
+        super();
+        this.message = message;
+        this.status = status;
+        console.error(this.stack);
+    }
+}
+
+module.exports = ExpressError;
